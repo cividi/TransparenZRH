e65f5e82aead718b218390264b58801ba2feb0b6
SvenBrieden <sven.brieden@posteo.de>
2021-04-01 14:42:36 +0200

    add pipeline for bike layout

--- api/index.py
+++ api/index.py
@@ -1,12 +1,107 @@
 from frictionless import Package, Layout, transform, steps
 import requests
 import datetime
+from dateutil import parser
 import json
 from flask import Flask, Response, request
 from flask_cors import CORS, cross_origin
 app = Flask(__name__)
 app.url_map.strict_slashes = False
 cors = CORS(app)
+
+
+def bike_layout_pipeline(sensor, url_Open_Data_Katalog):
+    filters = {"FK_ZAEHLER": sensor}
+    fields = ['VELO_IN', 'VELO_OUT', 'DATUM']
+    load_params = {'resource_id': 'ebe5e78c-a99f-4607-bedc-051f33d75318',
+                   'fields': ', '.join(fields),
+                   'filters': json.dumps(filters),
+                   'limit': 32000,
+                   }
+
+    response = requests.get(url_Open_Data_Katalog, params=load_params)
+    json_response = response.json()
+    if 'result' in json_response.keys():
+        json_results = json_response['result']
+        if len(json_results) < 1:
+            print('No results in response')
+        if 'records' in json_results.keys():
+            json_records = json_results['records']
+            if len(json_records) < 1:
+                print('No records in results')
+        else:
+            print('response.result not correct structure')
+    else:
+        print('response not correct structure')
+
+    counter_today, counter_yesterday, list_of_all_counts = 0, 0, []
+    today = datetime.date.today()
+    yesterday = today - datetime.timedelta(days=1)
+    for entry in json_records:
+        entrys_date = parser.isoparse(entry['DATUM']).date()
+        velo_in, velo_out = entry['VELO_IN'], entry['VELO_OUT']
+        if type(velo_in) != str:
+            velo_in = 0
+        if type(velo_out) != str:
+            velo_out = 0
+        if entrys_date == yesterday:
+            counter_yesterday += int(velo_in) + int(velo_out)
+        if entrys_date == today:
+            counter_today += int(velo_in) + int(velo_out)
+        list_of_all_counts.append(int(velo_in) + int(velo_out))
+
+    counter_year = sum(list_of_all_counts)
+    if len(list_of_all_counts) > 0:
+        mean_per_15Min_over_all = sum(
+            list_of_all_counts)/len(list_of_all_counts)
+        mean_per_day = int(mean_per_15Min_over_all * 24 * 60 / 15)
+    else:
+        mean_per_day = 'Kann zZt. noch nicht dargestellt werden.'
+
+    if counter_today == 0:
+        counter_today = 'Kann zZt. noch nicht dargestellt werden.'
+    if counter_yesterday == 0:
+        counter_yesterday = 'Kann zZt. noch nicht dargestellt werden.'
+    if counter_year == 0:
+        counter_year = 'Kann zZt. noch nicht dargestellt werden.'
+
+    return Response(
+        json.dumps({
+            "layout": 'bike',
+            "sensor": sensor,
+            "title": f"Velozählstelle  {sensor} ToDo:Renaming",
+            "description": "Das Tiefbauamt der Stadt Zürich erhebt seit 2009 die Velofrequenzen in der Stadt Zürich mit Hilfe automatischer Zählgeräte. Das Zählstellennetz umfasst derzeit 24 Zählgeräte. Die Messung erfolgt über im Boden eingelassene Induktionsschlaufen. Über Funk werden die Daten an einen Server übermittelt. Die Geräte sind vor dem Hintergrund des Datenschutzes unbedenklich, da lediglich Velofahrten gezählt und keine Daten über Nutzer oder Velos detektiert werden können. Die Daten der Zählgeräte werden via OpenData-Portal im Internet frei zugänglich gemacht.",
+            "updated": datetime.datetime.strptime(json_records[-1]['DATUM'], "%Y-%m-%dT%H:%M").isoformat(),
+            "gauges": [
+                {
+                    "label": 'Anzahl Velos gestern',
+                    "value": counter_yesterday,
+                    "unit": ''
+                },
+                {
+                    "label": 'Durchschnittliche Velos pro Tag',
+                    "value": mean_per_day,
+                    "unit": ''
+                },
+                {
+                    "label": 'Total Velos seit Jahresbeginn',
+                    "value": counter_year,
+                    "unit": ''
+                },
+                {
+                    "label": 'Anzahl Velos heute',
+                    "value": counter_today,
+                    "unit": ''
+                }
+            ],
+            "links":
+                {
+                "url": "https://data.stadt-zuerich.ch/dataset/ted_taz_verkehrszaehlungen_werte_fussgaenger_velo/resource/ebe5e78c-a99f-4607-bedc-051f33d75318",
+                "text": "Rohdaten auf data.stadt-zuerich.ch"
+            },
+        },),
+        mimetype='application/json',
+        status=200)
 
 
 def air_layout_pipeline(sensor, url_Open_Data_Katalog):
@@ -97,5 +192,7 @@ def api(layout, sensor):
 
     if layout == 'air':
         return air_layout_pipeline(sensor, url_Open_Data_Katalog)
+    elif layout == 'bike':
+        return bike_layout_pipeline(sensor, url_Open_Data_Katalog)
     else:
         return unknown_layout_pipeline(layout, sensor)
