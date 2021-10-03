290cf7f78ed76fae9f129ca6bfd9954296fd08e0
Thorben Westerhuys <thorben@cividi.ch>
2021-10-03 09:20:54 +0200

    excluded cache check

--- src/aggregators.py
+++ src/aggregators.py
@@ -22,43 +22,43 @@ try:
         '%a, %d %b %Y %X %Z')
     # Load Sensors from GeoData portal via geo.pipeline.yaml if file has been updated today
 
-    if source_time >= cached_time:
-        env = Environment(loader=FileSystemLoader('src/descriptors'))
-        template = env.get_template(f"geo.pipeline.yaml")
-        recipe = template.render(
-            params={
-                "thisyear": int(datetime.strftime(date.today(), "%Y"))
-            }
-        )
-        target = Pipeline(yaml.full_load(recipe)).run()
+    # if source_time >= cached_time:
+    env = Environment(loader=FileSystemLoader('src/descriptors'))
+    template = env.get_template(f"geo.pipeline.yaml")
+    recipe = template.render(
+        params={
+            "thisyear": int(datetime.strftime(date.today(), "%Y"))
+        }
+    )
+    target = Pipeline(yaml.full_load(recipe)).run()
 
-        # check if pipeline is valid
-        if not target.valid:
-            print(target)
-        else:
-            try:
-                # convert resource from pipeline into python dictionary
-                sensors = target.task.target.to_inline(dialect=dict(keyed=True))
-                for sensor in sensors:
-                    print(f"processing {sensor}...")
-                    template = env.get_template(f"bikepedestrian.pipeline.yaml")
-                    recipe = template.render(params = sensor)
-                    recipe_parsed = yaml.full_load(recipe)
-                    pipeline = Pipeline(recipe_parsed).run()
-                    
-                    # NOTE: currently not possible with descriptive table-write, as options other than path are not expanded correctly
-                    pipeline.task.target.write(
-                        path=f"s3://transparenzrh/bikepedestrian/{sensor['sensor_ref']}.csv",
-                        control=S3Control(acl="public-read")
-                    )
-
-                    if not pipeline.valid:
-                        print(pipeline)
-                    else:
-                        print("done")
-            except Exception as e:
-                print(f"An error occured during processing: {e}")
+    # check if pipeline is valid
+    if not target.valid:
+        print(target)
     else:
-        print("Source file not newer... Trying again soon.")
+        try:
+            # convert resource from pipeline into python dictionary
+            sensors = target.task.target.to_inline(dialect=dict(keyed=True))
+            for sensor in sensors:
+                print(f"processing {sensor}...")
+                template = env.get_template(f"bikepedestrian.pipeline.yaml")
+                recipe = template.render(params = sensor)
+                recipe_parsed = yaml.full_load(recipe)
+                pipeline = Pipeline(recipe_parsed).run()
+                
+                # NOTE: currently not possible with descriptive table-write, as options other than path are not expanded correctly
+                pipeline.task.target.write(
+                    path=f"s3://transparenzrh/bikepedestrian/{sensor['sensor_ref']}.csv",
+                    control=S3Control(acl="public-read")
+                )
+
+                if not pipeline.valid:
+                    print(pipeline)
+                else:
+                    print("done")
+        except Exception as e:
+            print(f"An error occured during processing: {e}")
+    # else:
+    #     print("Source file not newer... Trying again soon.")
 except Exception as e:
     print(e)
